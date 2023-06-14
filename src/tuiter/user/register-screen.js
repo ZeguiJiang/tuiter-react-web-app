ddf0719608ecc9e90df5ad4607e60814c3ab8488
ZeguiJiang <monarchgui@gmail.com>
2023-06-14 12:45:32 -0700

    submission6

--- src/tuiter/user/register-screen.js
+++ src/tuiter/user/register-screen.js
@@ -21,38 +21,72 @@ function RegisterScreen() {
 
   return (
     <div>
-      <h1>Register Screen</h1>
-      <div className="mt-2">
-        <label>Username</label>
-        <input
-          className="form-control"
-          type="text"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </div>
-      <div className="mt-2">
-        <label>Password</label>
-        <input
-          className="form-control"
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
-      <div className="mt-2">
-        <label>Email</label>
-        <input
-          className="form-control"
-          type="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-      </div>
-      <button className="btn btn-primary mt-2" onClick={handleRegister}>
+    <h1>
         Register
-      </button>
+    </h1>
+    <div className="mt-2">
+        <label>First name</label>
+        <input className="form-control" type="text" value={firstName} onChange={(e) =>
+        setFirstName( e.target.value)
+    }/>
     </div>
+    <div className="mt-2">
+        <label>Last name</label>
+        <input className="form-control" type="text" value={lastName} onChange={(e) =>
+        setLastName(e.target.value)
+    }/>
+    </div>
+    <div className="mt-2">
+        <label>Username</label>
+        <input className="form-control" type="text" value={username} onChange={(e) =>
+        setUsername(e.target.value)
+    }/>
+    </div>
+    <div className="mt-2">
+        <label>Password</label>
+        <input className="form-control" type="password" value={password} onChange={(e) =>
+        setPassword(e.target.value)
+    }/>
+    </div>
+    <div>
+        <button className="btn btn-primary mt-2" onClick={handleRegister}>Register</button>
+
+    </div>
+    
+</div>
+    // <div>
+    //   <h1>Register Screen</h1>
+    //   <div className="mt-2">
+    //     <label>Username</label>
+    //     <input
+    //       className="form-control"
+    //       type="text"
+    //       value={username}
+    //       onChange={(event) => setUsername(event.target.value)}
+    //     />
+    //   </div>
+    //   <div className="mt-2">
+    //     <label>Password</label>
+    //     <input
+    //       className="form-control"
+    //       type="password"
+    //       value={password}
+    //       onChange={(event) => setPassword(event.target.value)}
+    //     />
+    //   </div>
+    //   <div className="mt-2">
+    //     <label>Email</label>
+    //     <input
+    //       className="form-control"
+    //       type="email"
+    //       value={email}
+    //       onChange={(event) => setEmail(event.target.value)}
+    //     />
+    //   </div>
+    //   <button className="btn btn-primary mt-2" onClick={handleRegister}>
+    //     Register
+    //   </button>
+    // </div>
   );
 }
 
